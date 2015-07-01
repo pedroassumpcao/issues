75877b36dcaf7ec909f125e5c1f202aef8c29284
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2015-07-01 16:38:55 -0500

    Convert data to hashdict.

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -48,6 +48,7 @@ defmodule Issues.CLI do
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
+    |> convert_to_list_of_hashdicts
   end
 
   def decode_response({:ok, body}), do: body
@@ -55,6 +56,10 @@ defmodule Issues.CLI do
     {_, message} = List.keyfind(error, "message", 0)
     IO.puts "Error fetching from Github: #{message}"
     System.halt(2)
+  end
+
+  def convert_to_list_of_hashdicts(list) do
+    list |> Enum.map(&Enum.into(&1, HashDict.new))
   end
 
 end
