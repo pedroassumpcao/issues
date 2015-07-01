82ab298eb73fcdb29a0f43fa8a288a38775ef493
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2015-07-01 16:24:21 -0500

    Decode json response and set up github url in config file.

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,6 +1,7 @@
 defmodule Issues.GithubIssues do
 
   @user_agent  [{"User-agent", "Pedro Assumpcao pedro.assumpcao@gmail.com"}]
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
     issues_url(user, project)
@@ -9,17 +10,17 @@ defmodule Issues.GithubIssues do
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response(response) do
     case response do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, body}
+        {:ok, :jsx.decode(body)}
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         {:ok, "Not found :("}
       {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
+        {:error, :jsx.decode(reason)}
     end
   end
 
