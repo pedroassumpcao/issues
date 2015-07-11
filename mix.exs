77e043a4b92ef01eb40747ed944d24faf5543d2a
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2015-07-11 17:14:00 -0500

    Add ex_doc.

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Issues.Mixfile do
     [app: :issues,
      version: "0.0.1",
      elixir: "~> 1.0",
+     name: "Issues",
+     source: "https://github.com/pedroassumpcao/issues",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      escript: escript_config,
@@ -30,7 +32,8 @@ defmodule Issues.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 0.7.0"},
-      {:jsx, "~> 2.0.0"}
+      {:jsx, "~> 2.0.0"},
+      {:ex_doc, github: "elixir-lang/ex_doc"}
     ]
   end
 
